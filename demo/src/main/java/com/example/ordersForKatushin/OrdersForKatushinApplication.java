3bb0cfae8781b594c95b922fb8e27ad46a71482a
LordAtars <120113187+LordAtars@users.noreply.github.com>
2025-01-20 21:07:52 +0300

    first commit

---
+++ demo/src/main/java/com/example/ordersForKatushin/OrdersForKatushinApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordersForKatushin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersForKatushinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersForKatushinApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/ordersForKatushin/OrdersForKatushinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordersForKatushin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersForKatushinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
